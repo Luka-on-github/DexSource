67622639f392ed4f9accd10686acceaf5b3d0cd8
Luka22r <129538733+Luka-on-github@users.noreply.github.com>
2023-07-29 20:34:31 +0100

    Add files via upload

---
+++ aboutCMD.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import datetime
+time = datetime.datetime.now()
+datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!dev_", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Logged into Nationdex at", time)
+    try:
+        synced = await bot.tree.sync()
+        print(f'synced {len(synced)} command(s)')
+    except Exeption as e:
+        print(e)
+        
+@bot.tree.command(name="about")
+async def about(interaction: discord.Interaction):
+    embed=discord.Embed(title="About", description="<:Bosnia:1133834907362263090> <:England:1133834783793889290> <:Finland:1134043061190668360> <:Iraq:1133835397185687573> <:Ireland:1133834651786559628> <:Malaysia:1133835743329005710> <:Singapore:1133835155799281704> <:france:1133834989264441474> <:Afghanistan:1133835311005302934> \n \n Nationdex is a ballsdex similar like bot with more features and more wack! These include different types of balls for different seasons, a different spawning system, a econemy, quests, and a shop. \n If you want to find out more look bellow: \n Version: v2.31", color=0x7ca8db)
+    await interaction.response.send_message(embed=embed)
+  
+bot.run("MTA0ODA0MTUwODE2MDQyMTg4OA.GNKebu.n65JyyON6QgmFM03spnd7gkNKCdY_VKzN2RWH0")
